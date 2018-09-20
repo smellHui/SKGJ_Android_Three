d84e19b74330640377433bc0c836c2697154f32f
张鑫华-Android开发-武汉 <137330275@qq.com>
2018-09-20 14:36:49 +0800

    1、天气完善2

--- main/src/main/java/com/tepia/main/view/mainworker/yunwei/YunWeiXunChaFragment.java
+++ main/src/main/java/com/tepia/main/view/mainworker/yunwei/YunWeiXunChaFragment.java
@@ -1,6 +1,8 @@
 package com.tepia.main.view.mainworker.yunwei;
 
 
+import android.databinding.DataBindingUtil;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.view.View;
 
@@ -8,15 +10,22 @@ import com.alibaba.android.arouter.facade.annotation.Route;
 import com.tepia.base.AppRoutePath;
 import com.tepia.base.mvp.BaseCommonFragment;
 import com.tepia.main.R;
+import com.tepia.main.databinding.FragmentYunWeiXunchaBinding;
 
 /**
- * @author         :      zhang xinhua
+ * @author :      zhang xinhua
  * Version         :       1.0
  * 功能描述        :        主页二 运维页 巡查责任人
  **/
 @Route(path = AppRoutePath.app_main_fragment_yunwei)
 public class YunWeiXunChaFragment extends BaseCommonFragment {
 
+
+    private TabLayout tlTitle;
+    /**
+     *
+     */
+    private FragmentYunWeiXunchaBinding mBinding;
 
     public YunWeiXunChaFragment() {
         // Required empty public constructor
@@ -35,7 +44,10 @@ public class YunWeiXunChaFragment extends BaseCommonFragment {
 
     @Override
     protected void initView(View view) {
-      setCenterTitle(getString(R.string.main_yunwei));
+        setCenterTitle(getString(R.string.main_yunwei));
+        getRightTianqi().setVisibility(View.VISIBLE);
+        tlTitle = (TabLayout)view.findViewWithTag("tl_title");
+        mBinding = DataBindingUtil.bind(view);
     }
 
     @Override
