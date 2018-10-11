ec995599aa97abd6a6ab35d124b667f0252c426d
张鑫华-Android开发-武汉 <137330275@qq.com>
2018-10-11 17:13:25 +0800

    1、添加时间选择 运维模块 巡查责任人

--- main/src/main/java/com/tepia/main/view/mainworker/yunwei/yunweilist/YunWeiListFragment.java
+++ main/src/main/java/com/tepia/main/view/mainworker/yunwei/yunweilist/YunWeiListFragment.java
@@ -12,6 +12,7 @@ import android.widget.AdapterView;
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.loadmore.SimpleLoadMoreView;
+import com.jzxiang.pickerview.data.Type;
 import com.tepia.base.AppRoutePath;
 import com.tepia.base.mvp.MVPBaseFragment;
 import com.tepia.base.utils.DoubleClickUtil;
@@ -24,9 +25,15 @@ import com.tepia.main.model.detai.ReservoirBean;
 import com.tepia.main.model.task.bean.TaskBean;
 import com.tepia.main.model.task.response.TaskListResponse;
 import com.tepia.main.model.user.UserManager;
+import com.tepia.main.utils.TimePickerDialogUtil;
 
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -42,6 +49,9 @@ public class YunWeiListFragment extends MVPBaseFragment<YunWeiListContract.View,
     private String selectedYunWeiType;
     public String defaultYunweiType;
     private ArrayList<String> yunweiTypeStrs;
+    private TimePickerDialogUtil timePickerDialogUtil;
+    private SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM", Locale.getDefault());
+
 
     @Override
     protected int getLayoutId() {
@@ -70,8 +80,49 @@ public class YunWeiListFragment extends MVPBaseFragment<YunWeiListContract.View,
             selectedYunWeiType = defaultYunweiType;
             mBinding.loSelectYunweiType.setVisibility(View.GONE);
         }
+        initMonthDate();
         initListener();
 
+    }
+
+    private void initMonthDate() {
+        long fiveYears = 5L * 365 * 1000 * 60 * 60 * 24L;
+        timePickerDialogUtil = new TimePickerDialogUtil(getActivity(), sf);
+        //得到一个Calendar的实例
+        Calendar ca = Calendar.getInstance();
+        //设置时间为当前时间
+        ca.setTime(new Date());
+        Date lastDate = ca.getTime();
+        mBinding.tvSelectMonth.setText(sf.format(lastDate));
+        mBinding.loSelectMonth.setOnClickListener(v -> {
+            String current = (String) mBinding.tvSelectMonth.getText();
+            long currentLong = 0;
+            if (current != null && current.length() > 0) {
+                currentLong = strToLong(current);
+            }
+            timePickerDialogUtil.initTimePickerSetStartAndEnd((timePickerView, millseconds) -> {
+                String text = timePickerDialogUtil.getDateToString(millseconds);
+                mBinding.tvSelectMonth.setText(text);
+            }, Type.YEAR_MONTH, System.currentTimeMillis() - fiveYears, System.currentTimeMillis(), R.color.color_load_blue);
+            if (timePickerDialogUtil.startDialog != null) {
+                timePickerDialogUtil.startDialog = null;
+            }
+            if (currentLong != 0) {
+                timePickerDialogUtil.builder.setCurrentMillseconds(currentLong);
+            }
+            timePickerDialogUtil.builder.setTitleStringId("请选择查询月份");
+            timePickerDialogUtil.startDialog = timePickerDialogUtil.builder.build();
+            timePickerDialogUtil.startDialog.show(getFragmentManager(), "all");
+        });
+    }
+
+    public static long strToLong(String strDate) {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM");
+        ParsePosition pos = new ParsePosition(0);
+        Date strtodate = formatter.parse(strDate, pos);
+        //继续转换得到秒数的long型
+        long lTime = strtodate.getTime();
+        return lTime;
     }
 
     private void initListener() {
@@ -79,9 +130,9 @@ public class YunWeiListFragment extends MVPBaseFragment<YunWeiListContract.View,
             @Override
             public void onRefresh() {
                 if (selectedResrvoir == null) {
-                    mPresenter.getPatrolWorkOrderList("", selectedYunWeiType);
+                    mPresenter.getPatrolWorkOrderList("", selectedYunWeiType, mBinding.tvSelectMonth.getText().toString());
                 } else {
-                    mPresenter.getPatrolWorkOrderList(selectedResrvoir.getReservoirId(), selectedYunWeiType);
+                    mPresenter.getPatrolWorkOrderList(selectedResrvoir.getReservoirId(), selectedYunWeiType, mBinding.tvSelectMonth.getText().toString());
                 }
                 mBinding.srflContainer.setRefreshing(false);
             }
@@ -107,9 +158,9 @@ public class YunWeiListFragment extends MVPBaseFragment<YunWeiListContract.View,
             public void onClick(View view) {
 
                 if (selectedResrvoir == null) {
-                    mPresenter.getPatrolWorkOrderList("", selectedYunWeiType);
+                    mPresenter.getPatrolWorkOrderList("", selectedYunWeiType, mBinding.tvSelectMonth.getText().toString());
                 } else {
-                    mPresenter.getPatrolWorkOrderList(selectedResrvoir.getReservoirId(), selectedYunWeiType);
+                    mPresenter.getPatrolWorkOrderList(selectedResrvoir.getReservoirId(), selectedYunWeiType, mBinding.tvSelectMonth.getText().toString());
                 }
 
             }
@@ -123,7 +174,7 @@ public class YunWeiListFragment extends MVPBaseFragment<YunWeiListContract.View,
         adapterPatrolWorkOrderList.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
-                if (DoubleClickUtil.isFastDoubleClick()){
+                if (DoubleClickUtil.isFastDoubleClick()) {
                     return;
                 }
                 ARouter.getInstance().build(AppRoutePath.app_task_detail)
@@ -131,7 +182,6 @@ public class YunWeiListFragment extends MVPBaseFragment<YunWeiListContract.View,
                         .navigation();
             }
         });
-
 
 
         adapterPatrolWorkOrderList.openLoadAnimation();
@@ -145,9 +195,9 @@ public class YunWeiListFragment extends MVPBaseFragment<YunWeiListContract.View,
                     public void run() {
                         if (mPresenter.isCanLoadMore) {
                             if (selectedResrvoir == null) {
-                                mPresenter.getPatrolWorkOrderListMore("", selectedYunWeiType);
+                                mPresenter.getPatrolWorkOrderListMore("", selectedYunWeiType, mBinding.tvSelectMonth.getText().toString());
                             } else {
-                                mPresenter.getPatrolWorkOrderListMore(selectedResrvoir.getReservoirId(), selectedYunWeiType);
+                                mPresenter.getPatrolWorkOrderListMore(selectedResrvoir.getReservoirId(), selectedYunWeiType, mBinding.tvSelectMonth.getText().toString());
                             }
                         } else {
                             adapterPatrolWorkOrderList.loadMoreEnd();
@@ -161,7 +211,7 @@ public class YunWeiListFragment extends MVPBaseFragment<YunWeiListContract.View,
 
     @Override
     protected void initRequestData() {
-        mPresenter.getPatrolWorkOrderList("", selectedYunWeiType);
+        mPresenter.getPatrolWorkOrderList("", selectedYunWeiType, mBinding.tvSelectMonth.getText().toString());
     }
 
     private void showSelectYunweiType() {
@@ -182,8 +232,8 @@ public class YunWeiListFragment extends MVPBaseFragment<YunWeiListContract.View,
                 if (position != 0) {
                     if (yunweiTypeStrs != null) {
                         selectedYunWeiType = UserManager.getInstance().getYunWeiTypeList().get(yunweiTypeStrs.get(position)) + "";
-                    }else {
-                        selectedYunWeiType = position+"";
+                    } else {
+                        selectedYunWeiType = position + "";
                     }
                 } else {
                     selectedYunWeiType = "";
--- main/src/main/java/com/tepia/main/view/mainworker/yunwei/yunweilist/YunWeiListPresenter.java
+++ main/src/main/java/com/tepia/main/view/mainworker/yunwei/yunweilist/YunWeiListPresenter.java
@@ -8,6 +8,8 @@ import com.tepia.main.R;
 import com.tepia.main.model.task.TaskManager;
 import com.tepia.main.model.task.response.TaskListResponse;
 
+import java.util.Calendar;
+
 /**
  * MVPPlugin
  * 邮箱 784787081@qq.com
@@ -18,9 +20,15 @@ public class YunWeiListPresenter extends BasePresenterImpl<YunWeiListContract.Vi
     String pageSize = "10";
     public boolean isCanLoadMore = true;
 
-    public void getPatrolWorkOrderList(String reservoirId, String operationType) {
+    public void getPatrolWorkOrderList(String reservoirId, String operationType, String month) {
         String startDate = null;
         String endDate = null;
+        String[] split = month.split("-");
+        if (split.length == 2) {
+            int dayOfMonth = getDayOfMonth(Integer.valueOf(split[0]), Integer.valueOf(split[1]));
+            startDate = month + "-01 00:00:00";
+            endDate = month + "-" + dayOfMonth + " 23:59:59";
+        }
         currentPage = 1;
         boolean isShow = true;
         String msg = ResUtils.getString(R.string.data_loading);
@@ -43,10 +51,23 @@ public class YunWeiListPresenter extends BasePresenterImpl<YunWeiListContract.Vi
         });
     }
 
-    public void getPatrolWorkOrderListMore(String reservoirId, String operationType) {
+    private int getDayOfMonth(int year, int month) {
+        Calendar c = Calendar.getInstance();
+        c.set(year, month, 0); //输入类型为int类型
+        int dayOfMonth = c.get(Calendar.DAY_OF_MONTH);
+//        LogUtil.i("一个月的天数:"+dayOfMonth);
+        return dayOfMonth;
+    }
+
+    public void getPatrolWorkOrderListMore(String reservoirId, String operationType, String month) {
         String startDate = null;
         String endDate = null;
-
+        String[] split = month.split("-");
+        if (split.length == 2) {
+            int dayOfMonth = getDayOfMonth(Integer.valueOf(split[0]), Integer.valueOf(split[1]));
+            startDate = month + "-01 00:00:00";
+            endDate = month + "-" + dayOfMonth + " 23:59:59";
+        }
         boolean isShow = false;
         String msg = null;
         if (!isCanLoadMore) {
@@ -70,4 +91,6 @@ public class YunWeiListPresenter extends BasePresenterImpl<YunWeiListContract.Vi
             }
         });
     }
+
+
 }
