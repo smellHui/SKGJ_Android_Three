7de9fed2f092f976102e717ca4a88cb33a39e319
wwj <448224901@qq.com>
2019-01-15 14:23:48 +0800

    修改运维

--- main/src/main/java/com/tepia/main/view/mainadminister/yunwei/adapter/MyAdminOperationListAdapter.java
+++ main/src/main/java/com/tepia/main/view/mainadminister/yunwei/adapter/MyAdminOperationListAdapter.java
@@ -61,7 +61,7 @@ public class MyAdminOperationListAdapter extends BaseQuickAdapter<AdminWorkOrder
             tvReservoirName.setText(item.getAreaName()+"("+reservoirNum+")");
             int percent=0;
             if (doneRate!=0&&reservoirNum!=0){
-                percent = (int) (doneRate*100/reservoirNum);
+                percent = (int) (doneRate*100);
             }
             tvStatus.setText(percent+"%");
             tvComplete.setText(item.getDoneNum()+"");
